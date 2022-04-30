12a26fbda35115a85283ec7cda69dce20a432ce0
chanwon moon <kogoome@kakao.com>
2022-05-01 07:04:37 +0900

    네비 메뉴 카테고리화

--- src/frontend/nav.svelte
+++ src/frontend/nav.svelte
@@ -1,16 +1,24 @@
 <script lang="ts">
-  import type { navItem } from "../frontend/navItems";
-  export let navItems : Array<navItem>
+  import {categories, categoryItems} from './navItems';
   import {page} from '$app/stores'
   $: currentLink = $page.url.pathname
+
+  function getRandomColor() {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
 </script>
 
 <nav>
-  <ul class="flex flex-wrap list-none gap-6">
-    {#each navItems as navItem}
-      <li class="font-bold hover:text-[#52a7ec] transition-all duration-500">
-        <a class:active={currentLink === navItem.link} class="text-xl" href={navItem.link}>{navItem.title}</a>
+  <ul class="flex flex-wrap list-none gap-6 items-center">
+    {#each categories as category}
+      <li class="font-bold transition-all duration-500">
+        <div class="bg-[#52a7ec] rounded-2xl p-2 text-white">#{category} - </div>
       </li>
+      {#each categoryItems[category] as item}
+        <li class="font-bold hover:text-[#52a7ec] transition-all duration-500">
+          <a class:active={currentLink === item.link} class="text-xl" href={item.link}>{item.title}</a>
+        </li>
+      {/each}
     {/each}
   </ul>
 </nav>
