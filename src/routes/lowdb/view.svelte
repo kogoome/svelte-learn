df9cf45a0f84b6a5e54a5d92f08a3375aa4475a7
chanwon moon <kogoome@kakao.com>
2022-05-04 03:31:04 +0900

    엣지 정보에 노드 반영 클론 이용

--- src/routes/lowdb/view.svelte
+++ src/routes/lowdb/view.svelte
@@ -3,7 +3,8 @@
     const {nodeArray, edgeJson} = await fetch('/api/lowdb', {method: 'GET'})
     .then(res => res.json())
     .catch(err => console.log(err));
-    console.log(edgeJson);
+
+
     return {
       props:{nodeArray, edgeJson}
     }
@@ -12,10 +13,10 @@
 
 <script>
   import { fly, fade, slide } from 'svelte/transition';
-  /** @type {{ name: string[]; description: string[]; }[]} */
+  /** @type {{ name: string[]; description: string[]; _id:string }[]} */
   export let nodeArray
-  export let edgeJson
-  let states = {"english":false, "description":false, "nodeVisible":false}
+  export let edgeJson // 복사해서 아이디 검색한거 추가. 세이브는 원본에 아이디만 추가
+  let states = {"english":false, "description":false, "nodeVisible":false, "edgeVisible":false}
   $: langIndex = !states.english? 0 : 1 //#each 에서 사용
   let username = "kogoome"
 
@@ -89,6 +90,33 @@
     }
 
   }
+  const findNodeById = (_id) => {
+    return nodeArray.find(node=>node._id===_id)
+  }
+  const edgeJsonAddNodeArray = (_edgeJson) => {
+    const cloneEdgeJson = JSON.parse(JSON.stringify(_edgeJson))
+    // console.log("0", cloneEdgeJson)
+    for (let edge in cloneEdgeJson) {
+      // console.log("1 객체 순환 엣지 다루기 시작", ...cloneEdgeJson[edge])
+      cloneEdgeJson[edge] = cloneEdgeJson[edge].map(node => { // 카테고리 배열들에서 객체 뽑아내서
+        // console.log("2. 엣지 속 객체 순환")
+        const includes = node.includes.map(includeId=>{
+          // console.log("3. includes 객체반환")
+          return includeId?
+            findNodeById(includeId):null
+        })
+        // console.log("5. includes :", includes)
+        node = findNodeById(node._id)
+        node["includes"] = includes
+        // console.log("6. 변경된 node 확인 :",node)
+        return node
+        // 리턴해야하나? 수정했는데 해야할거같은데
+      })
+    }
+    // console.log("7", cloneEdgeJson)
+    return cloneEdgeJson
+  }
+  let cloneEdgeJson = edgeJsonAddNodeArray(edgeJson)
 </script>
 
 <!-- 상단 옵션 : 언어옵션 글로벌사용으로 전환시켜야함 -->
@@ -103,7 +131,7 @@
 
 <label>
 	<input type="checkbox" bind:checked={states.nodeVisible} class="hidden peer">
-	<div class="items-center peer-checked:bg-slate-100 transition-all duration-500">
+	<div class="flex gap-1 items-center peer-checked:bg-slate-100 transition-all duration-500">
     <span class="text-2xl">all nodes</span>
     <button class="btn btn-xs active:btn-accent" on:click={addNode}>add-node</button>
     <button class="btn btn-xs" on:click={descrive} id="desc" >description-off</button>
@@ -138,11 +166,29 @@
 
 
 
+<label>
+	<input type="checkbox" bind:checked={states.edgeVisible} class="hidden peer">
+	<div class="flex gap-1 items-center peer-checked:bg-slate-100 transition-all duration-500">
+    <span class="text-2xl">all edges</span>
+    <button class="btn btn-xs active:btn-accent" >add-edge</button>
+  </div>
+</label>
 
-<!-- 엣지 -->
-<div class="items-center">
-  <span class="text-2xl">all edges</span>
-  <button class="btn btn-xs active:btn-accent" >add-edge</button>
+{#if states.edgeVisible}
+<div transition:slide>
+  <div transition:fade> 
+    <!-- 엣지 이름 하드코딩됨 -->
+    <div class="text-lg">categories</div> 
+    {#each cloneEdgeJson.categories as category}
+      <div class="tooltip tooltip-right tooltip-info"
+      data-tip="{category.description[langIndex]}">
+        {category.name[langIndex]}{#if !states.english}({category.name[1]}){/if}
+      </div>
+      {#each category.includes as include}
+        <div>{include}</div>
+      {/each}
+    {/each}
+  </div>
 </div>
-{JSON.stringify(edgeJson)}
+{/if}
 
