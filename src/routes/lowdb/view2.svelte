d0221946be6fa5f22d3d37f154adecc518f278b5
kogoome <kogoome@kakao.com>
2022-05-07 15:35:41 +0900

    객체활용

--- src/routes/lowdb/view2.svelte
+++ src/routes/lowdb/view2.svelte
@@ -1,45 +1,69 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  onMount(() => {
-      const set = {
-      node:[],
-      edge:[]
-    }
-    const node = set["node"]
-    const edge = set["edge"]
+  type node = { [key: string]: {name:string[], type:string}, }
+  type edge = { [key: string]: {name:string[]}, }
+  interface nodeInfo {
+    id : string,
+    krName : string,
+    type : "node"
+  }
+  interface edgeInfo {
+    id : string,
+    krName : string,
+    type : "unary"|"binary"|"polynomial"
+  }
+  type set = { node:node, edge:edge }
 
-    function addNode(nodeInfo:nodeInfo):void {
-      const {id, krName, enDescription, krDescription, type }= nodeInfo
-      if (id) {
-        node[id] = {
-          name: [krName],
-          description: [enDescription, krDescription],
-          type
-        }
+
+  const set:set = {
+    node:{},
+    edge:{}
+  }
+  const {node, edge} = set
+
+  function addNode(nodeInfo:nodeInfo|edgeInfo):void {
+    const {id, krName, type }= nodeInfo
+    if (id) {
+      node[id] = {
+        name: [krName],
+        type
       }
     }
-
-    interface nodeInfo {
-      id : string,
-      krName : string,
-      enDescription : string,
-      krDescription : string,
-      type : "unary"|"binary"|"polynomial"
-    }
-
-    const mean : nodeInfo = {
-      id: "meaning",
-      krName: "의미",
-      enDescription: "context meaning",
-      krDescription: "문맥상의 의미",
-      type: "unary"
-    }
-    addNode(mean)
-    console.log(set)
   }
 
 
+
+  const mean : edgeInfo = {
+    id: "meaning",
+    krName: "의미",
+    type: "unary"
+  }
+
+  const categories : edgeInfo = {
+    id: "categories",
+    krName: "카테고리",
+    type: "polynomial"
+  }
+
+  const math : nodeInfo = {
+    id: "math",
+    krName: "수학",
+    type: "node"
+  }
+
+  addNode(mean)
+  addNode(categories)
+  addNode(math)
+  
+
+  console.log(set)
+
 </script>
 
-{JSON.stringify(set)}
 
+<h2>all nodes</h2>
+{#each Object.keys(node) as item}
+<div>
+  {node[item].name}({item})  
+</div>
+  
+{/each}
---
+++ src/routes/lowdb/viewArr.svelte
@@ -0,0 +1,9 @@
+<script>
+  const set = [
+    
+  ]
+
+
+
+
+</script>
