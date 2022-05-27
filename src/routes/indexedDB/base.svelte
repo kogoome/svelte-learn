53ebf9362cb2bcafd62805547c02b74d59e31df6
wistaria <kogoome@kakao.com>
2022-05-28 06:27:48 +0900

    진행중

--- src/routes/indexedDB/base.svelte
+++ src/routes/indexedDB/base.svelte
@@ -1,9 +1,25 @@
 <script>
+	import { fly, fade, slide } from 'svelte/transition';
+	// let accordionChecked = false
 </script>
 
-TODO 업무 분배용 칸반을 제작해보자
-<div class="flex flex-row">
-	<div class="bg-slate-300">a</div>
-	<div class="bg-slate-300">a</div>
-	<div class="bg-slate-300">a</div>
+<h1 class="text-center">TODO 업무 진행용 칸반을 제작해보자</h1>
+
+<div class="flex rounded-xl bg-slate-200 p-3 m-3 justify-between gap-3">
+	<div class="basis-1/3 w-full bg-slate-300 rounded-md">
+		<h1 class="text-center">Todo</h1>
+
+		<h1>philosophy</h1>
+		make highquality education share platform structured knowledge make knowledge relation freely education
+		out of the world
+		<h1>x.0.0</h1>
+		<h1>0.y.0</h1>
+		<h1>0.0.z</h1>
+	</div>
+	<div class="basis-1/3 w-full bg-slate-400 rounded-md">
+		<h1 class="text-center">Process</h1>
+	</div>
+	<div class="basis-1/3 w-full bg-slate-500 rounded-md">
+		<h1 class="text-center">Complete</h1>
+	</div>
 </div>
