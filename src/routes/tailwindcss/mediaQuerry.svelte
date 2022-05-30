6d996442c69ab5677b1ca3b4ce4370e230cb5189
wistaria <kogoome@kakao.com>
2022-05-30 20:00:22 +0900

    미디어쿼리 테일윈드

---
+++ src/routes/tailwindcss/mediaQuerry.svelte
@@ -0,0 +1,12 @@
+<div>ctrl 휠: 크기조정</div>
+
+<div
+  class="h-20 w-20 bg-black sm:bg-blue-500 md:bg-green-500 lg:bg-red-500 xl:bg-amber-500 2xl:bg-cyan-500"
+/>
+
+<div class="bg-black text-white">normal : bg-black</div>
+<div class="bg-blue-500">sm : bg-blue-500</div>
+<div class="bg-green-500">md : bg-green-500</div>
+<div class="bg-red-500">lg : bg-red-500</div>
+<div class="bg-amber-500">xl : bg-amber-500</div>
+<div class="bg-cyan-500">2xl : bg-cyan-500</div>
