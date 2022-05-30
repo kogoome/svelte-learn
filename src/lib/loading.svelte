e8659e0abc3052f62c00cc4f558394fd83b75338
wistaria <kogoome@kakao.com>
2022-05-30 19:59:39 +0900

    로딩 정보

---
+++ src/lib/loading.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { loading } from '$lib/loading'
+</script>
+
+{#if $loading}
+  <div class="fixed h-screen w-screen flex justify-center items-center z-50">
+    <div class="bg-purple-400 rounded-full h-20 w-20 " />
+  </div>
+{/if}
