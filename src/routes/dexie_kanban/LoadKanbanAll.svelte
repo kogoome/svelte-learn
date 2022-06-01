d221ca163282db3f89898644c65b0d727ac8556c
wistaria <kogoome@kakao.com>
2022-06-02 05:34:28 +0900

    수정

--- src/routes/dexie_kanban/LoadKanbanAll.svelte
+++ src/routes/dexie_kanban/LoadKanbanAll.svelte
@@ -1,21 +1,16 @@
 <script lang="ts">
   import { liveQuery, type Observable } from 'dexie'
-  import { onMount } from 'svelte'
   import { db, type Kanban } from './db'
+  import { fly, fade, slide, blur, scale, draw, crossfade } from 'svelte/transition'
 
-  let Todo: Observable<Kanban[]> | Kanban[]
-  Todo = liveQuery(() => db.Todo.reverse().toArray())
-  let Process = liveQuery(() => db.Process.reverse().toArray())
-  let Complete = liveQuery(() => db.Complete.reverse().toArray())
-
-  onMount(() => {
-    console.log(Todo)
-  })
+  $: Todo = liveQuery(() => db.Todo.reverse().toArray())
+  $: Process = liveQuery(() => db.Process.reverse().toArray())
+  $: Complete = liveQuery(() => db.Complete.reverse().toArray())
 </script>
 
 <!-- 오류 우에 잡노.. -->
 {#if $Todo}
-  <div class="bg-slate-200 rounded-3xl p-2">
+  <div class="bg-slate-200 rounded-3xl p-2" transition:blur={{ duration: 700 }}>
     <div class="bg-slate-300 rounded-2xl pt-3 pb-7">
       <h1 class="w-full bg-white flex justify-center">Todo</h1>
       {#each $Todo as taskObj (taskObj.index)}
