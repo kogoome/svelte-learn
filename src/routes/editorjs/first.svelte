bea5d1d6f6125b9345d883a0298b1c4db10ed028
wistaria <kogoome@kakao.com>
2022-06-04 23:31:49 +0900

    에디터js 작성 학습대상

---
+++ src/routes/editorjs/first.svelte
@@ -0,0 +1,21 @@
+<script>
+  import EditorJS from '@editorjs/editorjs'
+  import Header from '@editorjs/header'
+  import List from '@editorjs/list'
+
+  const editor = new EditorJS({
+    holder: 'editorjs', // div id
+    tools: {
+      header: {
+        class: Header,
+        inlineToolbar: ['link']
+      },
+      list: {
+        class: List,
+        inlineToolbar: true
+      }
+    }
+  })
+</script>
+
+<div id="editorjs" />
