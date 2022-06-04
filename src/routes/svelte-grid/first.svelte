8c70b2aa95d9f56bde32b625a437544faa1cd5a0
wistaria <kogoome@kakao.com>
2022-06-04 23:32:12 +0900

    수정

--- src/routes/svelte-grid/first.svelte
+++ src/routes/svelte-grid/first.svelte
@@ -24,7 +24,7 @@
   })
 
   function generateLayout(col: number) {
-    return new Array(15).fill(null).map(function (item, i) {
+    return new Array(categories.length).fill(null).map(function (item, i) {
       const x = Math.ceil(Math.random() * 4) + 2
       const y = Math.ceil(Math.random() * 3) + 1
       return {
@@ -41,19 +41,89 @@
 </script>
 
 <svelte:head>
-  <title>Svelte-grid</title>
+  <title>index page</title>
 </svelte:head>
-
-<div class="bg-slate-200 rounded-xl m-10 py-3 shadow-lg">
-  <div class="bg-white text-center mb-3">
-    <h1>Svelte-grid</h1>
-  </div>
-  <Grid bind:items {cols} rowHeight={100} let:dataItem fillSpace={fillFree}>
-    <div
-      class="h-full w-full rounded-lg opacity-80 shadow-md"
-      style="background-image: linear-gradient({dataItem.data.start}, {dataItem.data.end});"
-    >
-      {@html dataItem.content}
+<div class="flex justify-center">
+  <div class="container rounded-xl m-10 py-3 shadow-lg text-center ">
+    <div class="bg-white text-center mb-3">
+      <h1>Feature index page</h1>
+      with svelte-grid
     </div>
-  </Grid>
+    <div class="gridbox m-auto">
+      <Grid bind:items {cols} rowHeight={100} let:dataItem fillSpace={fillFree}>
+        <div
+          class="h-full w-full rounded-lg opacity-80 shadow-md"
+          style="background-image: linear-gradient({dataItem.data.start}, {dataItem.data.end});"
+        >
+          {@html dataItem.content}
+        </div>
+      </Grid>
+    </div>
+  </div>
 </div>
+
+<style>
+  @-webkit-keyframes AnimationName {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+  @-moz-keyframes AnimationName {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+  @-o-keyframes AnimationName {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+  @keyframes AnimationName {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+
+  .container {
+    background: linear-gradient(
+      -85deg,
+      #ffd6d6,
+      #c2fff9,
+      #fcffd6,
+      #c5ffc2,
+      #d6e3ff,
+      #f3c2ff,
+      #ffd6ef,
+      #c2daff
+    );
+    background-size: 400% 400%;
+    animation: AnimationName 30s ease infinite;
+  }
+  .gridbox {
+    width: 800px;
+  }
+</style>
