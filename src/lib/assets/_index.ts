90ce6f29335db405f6d54765b782e2ac0c91efc2
wistaria <kogoome@kakao.com>
2022-06-08 17:21:55 +0900

    버튼 이미지

---
+++ src/lib/assets/_index.ts
@@ -0,0 +1,30 @@
+import trash from './trash.png'
+import writing from './writing.png'
+import sla from './sla.png'
+import save from './save.png'
+import satisfaction from './satisfaction.png'
+import comment from './comment.png'
+import colorPalette from './color-palette.png'
+import chat from './chat.png'
+import addUser from './add-user.png'
+import tenMinutes from './10-minutes.png'
+import info from './info.png'
+import contract from './contract.png'
+import expand from './expand.png'
+import clock from './clock.png'
+export {
+  clock,
+  expand,
+  contract,
+  trash,
+  writing,
+  sla,
+  info,
+  save,
+  satisfaction,
+  comment,
+  colorPalette,
+  chat,
+  addUser,
+  tenMinutes
+}
