2c0388ef2bf2a97d73e9a66a25d4e79559b5f912
wistaria <kogoome@kakao.com>
2022-06-22 23:22:32 +0900

    함수를 모듈화할수는 없나

--- src/routes/dataStructure/moduleFn.svelte
+++ src/routes/dataStructure/moduleFn.svelte
@@ -1,11 +1,5 @@
 <script>
-  function custom1(...args) {
-    let result = functionJson.name
-    console.log(result)
-  }
-  let functionJson = {
-    name: () => {
-      return 'name'
-    }
-  }
 </script>
+
+함수가 포함된 스벨트 파일을 함수의 매개변수와 인수 내용을 모두 넘기고 모듈화 할 수 있는 방법은
+뭐가있나
